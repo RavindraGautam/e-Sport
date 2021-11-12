6b117da987ea4dda8beb7e44a7c4ae9809af63fe
Ravindra Kumar Gautam <83901187+RavindraGautam@users.noreply.github.com>
2021-11-12 22:11:14 +0530

    This is home page.

---
+++ home.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>e-Sport</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<header>
+
+	<div class="bg-img">
+
+		<nav class="navbar navbar-expand-md bg-light navbar-light">
+		  <!-- Brand -->
+		  <a class="navbar-brand text-success" href="images/logonew.png"><img src="images/logonew.png" style="height:50px;width:auto;">
+		  Sport e-Management</a>
+
+		  <!-- Toggler/collapsibe Button -->
+		  <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <!-- Navbar links -->
+		  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+			<ul class="navbar-nav ml-auto">
+			  <li class="nav-item">
+				<a class="nav-link active" href="#">Home</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="aboutus.html">About Us</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="admin/Sport/view.php">View Game</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="SportRegister.html">Register</a>
+			  </li>
+			</ul>
+		  </div>
+		</nav>
+		
+		
+		
+		<section class="my-5">
+			<div class="py-4">
+				<div class="container-fluid">
+				<div class="row">
+				<div class="col-lg-6 col-md-6 col-12 text-center">
+					<h2>Welcome To</h2>
+					<h2>Sport e-Management System</h2>
+					<p class="py-5 text-light text-center" style="font-size:25px;"> If any student wants to participate in any game
+					</p>
+					<a href="SportRegister.html" class="btn btn-warning">Register here</a>
+				</div>
+			</div>
+		</section>	
+		
+		
+		
+		
+	</div>	
+</header>
+
+
+<section class="my-4">
+	<div class="py-5">
+		<h3 class="text-center text-primary">About Us</h3>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-12">
+				<img src="images/sports.jpg" class="img-fluid aboutimg">
+			</div>
+			<div class="col-lg-6 col-md-6 col-12">
+				<h2 class="text-center display-5">Sport e-Management System</h2>
+				<p class="py-4"> We all know that in every college every year sport event organizes.
+				Due to this, the sports teacher or other staff who manage this event have many problems 
+				and they have to do hard work and time is also used to do a lot of paper work.
+				And student time also goes. 
+				Our project overcome all these problem. Sport teacher or 
+				other staff can do all these tasks from their mobile or system. 
+				</p>
+				<a href="aboutus.html" class="btn btn-success">check here</a>
+			</div>
+		</div>
+	</div>	
+</section>
+
+
+<footer>
+	<p class="p-3 text-center text-white bg-dark">&copy Sport e-Manegment System
+	<a type="button" class="btn btn-outline-light btn-sm mx-4" href="admin/login.php">Admin Login</a></p>
+</footer>
+
+</body>
+</html>
